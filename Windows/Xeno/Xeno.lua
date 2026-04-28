e1e8e6c333f0f40468346aeb78d7abcad2a1f2c8
kamui_kamikaze <pulsar.hub.developer@gmail.com>
2026-04-28 22:17:26 +0900

    UwU
    UwU

---
+++ Windows/Xeno/Xeno.lua
@@ -0,0 +1,11 @@
+--[[ Xeno Table ]]--
+Xeno.LockModule
+Xeno.UnlockModule
+Xeno.about
+Xeno.getfunctions
+Xeno.getinstanceaddress
+Xeno.getinstancefunctions
+Xeno.getproperties
+Xeno.getrbxscriptsignals
+Xeno.getrbxsignals
+Xeno.hookinstance
